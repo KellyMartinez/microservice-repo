664872112e5970677c92b9baabd4e9cd01226dd9
KellyMartinez <kellynhamartinez@hotmail.com>
2023-08-30 19:44:40 -0400

    Add files via upload
    Adcionando arquivos

---
+++ auth/src/test/java/com/microservices/AuthApplicationTests.java
@@ -0,0 +1,14 @@
+/*package com.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
